4a34d6c6943cc0b7b80fd5473c2997f9c45ef25b
xieyuxing <185246042@qq.com>
2022-10-11 14:09:12 +0800

    s

---
+++ cloud-eureka-server7001/src/main/java/com/lingyun/study/springcloud/EurekaMain7001.java
@@ -0,0 +1,21 @@
+package com.lingyun.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 implements ApplicationListener<ContextRefreshedEvent> {
+        public static void main(String[] args){
+
+                SpringApplication.run(EurekaMain7001.class, args);
+        }
+
+        @Override
+        public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        }
+}
